3a5346acf230d27ee6bbebfe3a6b20a5b1c359b4
ovisual <106680639+ovisual@users.noreply.github.com>
2024-03-30 23:06:33 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,34 @@ const dropdown=document.querySelector(".dropdown")
 const dropdown_menu=document.querySelector(".dropdown-menu")
 const catlink=document.querySelector(".cat-link")
 
+const nav=document.querySelector(".nav-links")
+const burger=document.querySelector(".burger")
+const close=document.querySelector(".close-icon")
+
+
+burger.addEventListener("click", ()=>{
+    nav.style.display="flex"
+    burger.style.display="none"
+    close.style.display="block"
+
+
+
+}
+)
+close.addEventListener("click",()=>{
+    nav.style.display="none"
+    burger.style.display="block"
+    close.style.display="none"
+
+
+
+    
+})
+
+
+
+
+
 
 
 
